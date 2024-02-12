5e09283d2faa4bab47953c809552633e11210356
Saud Ahmed <you@example.com>
2024-02-12 00:56:32 -0600

    Saud Ahmed - Base Setup

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -5,8 +5,8 @@ from flask import render_template
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', title = "Home")
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title ="About")
