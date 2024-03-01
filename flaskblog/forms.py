1d6630c85999b020eecc44ac404d1fec14fcf3d6
Saud Ahmed <you@example.com>
2024-03-01 17:25:14 -0600

    Saud Ahmed - Changes Made 3/1

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+from flaskblog.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -13,6 +14,16 @@ class RegistrationForm(FlaskForm):
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
+    def validate_username(self,username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is already taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        email = User.query.filter_by(username=email.data).first()
+        if email:
+            raise ValidationError('That email is already taken. Please choose a different one.')
+        
 class LoginForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
